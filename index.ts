a3813cf5422386bbdbc9f4d2a9a0b591f7ded6b8
Orkun <orkun@promega.com.tr>
2021-09-06 16:15:10 +0300

    FIX Index Exports

--- index.ts
+++ index.ts
@@ -1,33 +1,39 @@
-export * from './src/events/social.room.events';
+export {SocialRoomEvents} from './src/events/social.room.events';
 
-export * from './src/http/http.handler';
-export * from './src/http/middleware/get.user.middleware';
+export {SocialColyseusApp, AppOptions, DatabaseOptions} from './src/app';
 
-export * from './src/rooms/social.room';
+export {HttpHandler} from './src/http/http.handler';
+export {default as getUserMiddleware} from './src/http/middleware/get.user.middleware';
 
-export * from './src/services/auth/auth.service';
-export * from './src/services/auth/models/create.auth.model';
-export * from './src/services/auth/models/login.model';
-export * from './src/services/auth/models/register.model';
-export * from './src/services/auth/module/auth.module';
-export * from './src/services/auth/schema/auth.schema';
-export * from './src/services/auth/authenticate';
+export {SocialRoom} from './src/rooms/social.room';
 
-export * from './src/services/friendship/friendship.service';
-export * from './src/services/friendship/models/approve.friendship.model';
-export * from './src/services/friendship/models/create.friendship.model';
-export * from './src/services/friendship/models/delete.friendship.model';
-export * from './src/services/friendship/models/upsert.friendship.model';
-export * from './src/services/friendship/module/friendship.module';
-export * from './src/services/friendship/schema/friendship.schema';
+export {AuthService} from './src/services/auth/auth.service';
+export {CreateAuthModel} from './src/services/auth/models/create.auth.model';
+export {LoginModel, LoginValidator} from './src/services/auth/models/login.model';
+export {RegisterValidator, RegisterModel} from './src/services/auth/models/register.model';
+export {default as AuthModule} from './src/services/auth/module/auth.module';
+export {Auth, default as authSchema} from './src/services/auth/schema/auth.schema';
+export {default as authenticate} from './src/services/auth/authenticate';
 
-export * from './src/services/invitation/invitation.service';
-export * from './src/services/invitation/models/create.invitation.model';
-export * from './src/services/invitation/models/delete.invitation.model';
-export * from './src/services/invitation/models/delete.invitation.model';
-export * from './src/services/invitation/schema/invitation.schema';
+export {FriendshipService} from './src/services/friendship/friendship.service';
+export {ApproveFriendshipModel} from './src/services/friendship/models/approve.friendship.model';
+export {CreateFriendshipModel} from './src/services/friendship/models/create.friendship.model';
+export {DeleteFriendshipModel} from './src/services/friendship/models/delete.friendship.model';
+export {
+    friendshipExistsValidator,
+    default as UpsertFriendshipModel,
+    friendshipApproveValidator,
+    friendshipExistsQueryValidator,
+} from './src/services/friendship/models/upsert.friendship.model';
+export {default as FriendshipModule} from './src/services/friendship/module/friendship.module';
+export {Friendship, default as friendshipSchema} from './src/services/friendship/schema/friendship.schema';
 
-export * from './src/services/user/user.service';
-export * from './src/services/user/models/user.create.model';
-export * from './src/services/user/module/user.module';
-export * from './src/services/user/schema/user.schema';
+export {InvitationService} from './src/services/invitation/invitation.service';
+export {CreateInvitationModel} from './src/services/invitation/models/create.invitation.model';
+export {DeleteInvitationModel} from './src/services/invitation/models/delete.invitation.model';
+export {Invitation, default as invitationSchema} from './src/services/invitation/schema/invitation.schema';
+
+export {UserService} from './src/services/user/user.service';
+export {UserCreateModel} from './src/services/user/models/user.create.model';
+export {default as UserModule} from './src/services/user/module/user.module';
+export {User, default as userSchema} from './src/services/user/schema/user.schema';
